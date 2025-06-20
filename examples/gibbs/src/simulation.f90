8beed2e1a993f0b3d4a0407c2bc2cc01013fc716
Shayan <sh2278@cornell.edu>
2025-06-20 16:37:05 -0400

    Reading species information from gri30.yaml

--- examples/gibbs/src/simulation.f90
+++ examples/gibbs/src/simulation.f90
@@ -1,69 +1,77 @@
 !> Example to read in YAML thermodynamic file
 module simulation
    use precision, only: WP
-   !use ceq_types, only: sys_type
-   use YAMLRead
+   use YAMLRead,  only: YAMLElement
    implicit none
    private
    
-   ! YAML-related variables   
-   type(YAMLHandler) :: domain  ! be sure to close
-   type(YAMLMap) :: parent
-   type(YAMLMap) :: mapper_1
-   type(YAMLMap) :: mapper_2
-   integer :: n = -9999
-   integer :: i = -9999
-   integer :: code
+   ! The array of the species. Each stored as a YAMLElement object
+   type(YAMLElement), dimension(:), allocatable :: species
 
+   ! Simulation subroutines
    public :: simulation_init,simulation_run,simulation_final
    
 contains
 
-   !> Nothing to initialize
+
+   !> Initialization of problem solver
    subroutine simulation_init
-      use param,  only: param_read
-      use string, only: str_medium
+      use param,    only: param_read,param_getsize
+      use string,   only: str_short,str_medium
+      use messager, only: die
+      use YAMLRead, only: YAMLHandler,YAMLSequence,yaml_open_file,yaml_start_from_sequence,yaml_close_file
       implicit none
       character(len=str_medium) :: yaml_file
+      character(len=str_short), dimension(:), allocatable :: sp_names
+      type(YAMLHandler)  :: domain
+      type(YAMLSequence) :: sp_list
+      type(YAMLElement)  :: sp
+      real(WP), allocatable :: thermo_in(:,:)
+      integer :: n_species,nsc,n,i,code
+      character(len=:), allocatable :: name_arr(:)
+      character(len=:), allocatable :: name
 
+      ! Get the target species from input
+      n_species=param_getsize('Species')
+      allocate(sp_names(1:n_species))
+      call param_read('Species',sp_names)
+      
+      ! Read the mechanism file
       call param_read('YAML file',yaml_file)
-      print*,yaml_file
-      domain = yaml_open_file(trim(yaml_file))
-      parent = yaml_start_from_map(domain, "parent")
 
-      mapper_1 = parent%value_map("child1")
-      mapper_2 = parent%value_map("child_22222")
+      ! Get all the species from the mechanism
+      domain=yaml_open_file(trim(yaml_file))
+      sp_list=yaml_start_from_sequence(domain,'species')
 
-      write(*,*) "mapper 1 = ", mapper_1%value_str("boolean_flag", code)
-      write(*,*) "mapper 1 = ", mapper_1%value_double_1d("an_aray", code)
-      write(*,*) "mapper 1 = ", mapper_1%value_int_2d("array_int_2d", code)
-      write(*,*) ""
-      write(*,*) "mapper 2 = ", mapper_2%value_str("boolean_flag", code)
-      write(*,*) "mapper 2 = ", mapper_2%value_double_1d("an_aray", code)
-      write(*,*) "mapper 2 = ", mapper_2%value_double_2d("array_float_2d", code)
-
-      n = size(mapper_1%labels)
-      write(*,*) ""//achar(13)//achar(10)//"Mapper 1 keys:"
-      do i = 1,n
-         write(*,*) "  ", mapper_1%labels(i)%str
+      ! Extract the target species from the mechanism
+      allocate(species(1:n_species))
+      n=0
+      do nsc=0,sp_list%size-1 ! Index in YAMLSequence starts from 0
+         sp=sp_list%element(nsc)
+         name_arr=sp%value_str('name',code)
+         name=''
+         do i=1,size(name_arr)
+            name=trim(name//name_arr(i))
+         end do
+         if (any(sp_names.eq.name)) then
+            n=n+1
+            species(n)=sp
+         end if
+         call sp%destroy()
       end do
+      if(n.ne.n_species) call die('Some species are missing in the mechanism file.')
 
-      call mapper_1%destroy()
-
-      n = size(mapper_2%labels)
-      write(*,*) ""//achar(13)//achar(10)//"Mapper 2 keys:"
-      do i = 1,n
-         write(*,*) "  ", mapper_2%labels(i)%str
-      end do
-
-      call parent%destroy()
-      call mapper_2%destroy()
+      ! Close the mechanism file
       call yaml_close_file(domain)
+
+      ! Read the NASA-7 polynomials
+      allocate(thermo_in(n_species,15))
+      thermo_in=0.0_WP
 
    end subroutine simulation_init
 
 
-   !> NOthing here yet
+   !> Perform an NGA2 simulation - this mimicks NGA's old time integration for multiphase
    subroutine simulation_run
       implicit none
 
@@ -76,7 +84,7 @@ contains
    subroutine simulation_final
       implicit none
       
-      ! Get rid of all objects - need destructors
+      ! Get rid of all objects-need destructors
       
    end subroutine simulation_final
 
